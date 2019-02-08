1711fcd0590f2fe30c7c22892a22d12ede107d9a
Daniel (dmilith) Dettlaff <dmilith@me.com>
2019-02-08 13:53:28 +0100

    new: Curl + HTTP2 test.

--- src/tests.rs
+++ src/tests.rs
@@ -3,13 +3,23 @@
 mod tests {
 
     // Load all internal modules:
-    use hyper::*;
+    // use hyper::*;
     use gotham::test::TestServer;
     use regex::Regex;
     use ssl_expiration::SslExpiration;
     use curl::easy::{Easy2, Handler, WriteError};
     use crate::*;
     use crate::web::router;
+
+
+    struct CollectorForTests(Vec<u8>);
+
+    impl Handler for CollectorForTests {
+        fn write(&mut self, data: &[u8]) -> Result<usize, WriteError> {
+            self.0.extend_from_slice(data);
+            Ok(data.len())
+        }
+    }
 
 
     #[test]
@@ -21,4 +31,20 @@ mod tests {
     }
 
 
+    #[test]
+    fn test_curl_basic_test() {
+        let domain = "https://www.google.com/";
+        let mut easy = Easy2::new(CollectorForTests(Vec::new()));
+        easy.get(true).unwrap();
+        easy.verbose(true).unwrap();
+        easy.url("https://www.rust-lang.org/").unwrap();
+        easy.perform().unwrap();
+        assert_eq!(easy.response_code().unwrap(), 200);
+        let contents = easy.get_ref();
+        let raw_page = String::from_utf8_lossy(&contents.0);
+        assert!(raw_page.contains("Rust"));
+        assert!(raw_page.contains("<meta "));
+        assert!(raw_page.contains("<head>"));
+        assert!(raw_page.contains("<body>"));
+    }
 }
