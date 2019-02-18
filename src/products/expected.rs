075060c417854ed702b44c19deab82b340569db6
Daniel (dmilith) Dettlaff <dmilith@me.com>
2019-02-18 01:14:42 +0100

    new: Defined Expected enum with expected successes.

--- src/products/expected.rs
+++ src/products/expected.rs
@@ -54,3 +54,35 @@ impl Default for DomainExpectation {
 
 /// Domain expectations type
 pub type DomainExpectations = Vec<DomainExpectation>;
+
+
+
+#[derive(Debug, Clone, Serialize, Deserialize, Fail, PartialEq)]
+/// All response types for all supported expectations
+pub enum Expected {
+
+
+    /// Check returned expected HTTP error code
+    #[fail(display = "URL: {} returned expected error-code: {}.", _0, _1)]
+    HttpCodeValid (String, u32),
+
+
+    /// Check returned expected page contents
+    #[fail(display = "URL: {} contains expected value: {}.", _0, _1)]
+    ContentValid (String, String),
+
+
+    /// Check returned expected page content length
+    #[fail(display = "URL: {} returned expected content-length at least: {} bytes long.", _0, _1)]
+    ContentLength (String, usize),
+
+
+    /// Check TLS certificate expiration time
+    #[fail(display = "TLS certificate for domain: {}, will be valid for: {} more days. Requested minimum: {} days.", _0, _1, _2)]
+    TLSCertificateFresh (String, i32, i32),
+
+
+}
+
+
+
