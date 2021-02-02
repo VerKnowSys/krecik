092244d06573385da61b7c0a5b982cfd96e89648
Daniel (dmilith) Dettlaff <daniel@centra.com>
2021-02-03 00:08:18 +0100

    imp: Several improvements to error contents of unexpected module.

--- src/products/unexpected.rs
+++ src/products/unexpected.rs
@@ -6,23 +6,24 @@ pub enum UnexpectedMinor {
     OSError(String, String),
 
     /// Failed internal function
-    #[fail(
-        display = "InternalProtocolProblemFailure on: {}. Details: {}.",
-        _0, _1
-    )]
+    #[fail(display = "InternalProtocolProblemFailure on: {}. Details: {}", _0, _1)]
     InternalProtocolProblem(String, String),
+
+    /// Curl multi handler minor failure
+    #[fail(display = "{}", _0)]
+    HandlerFailed(String),
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize, Fail, PartialEq, Eq, Hash)]
 /// Unexpected check result
 pub enum Unexpected {
     /// Failed to pass page expectation
-    #[fail(display = "Failed to connect to URL: {}. Details: {}.", _0, _1)]
+    #[fail(display = "Failed to connect to URL: {}. Details: {}", _0, _1)]
     URLConnectionProblem(String, String),
 
     /// Failed to pass page expectation
     #[fail(
-        display = "Expired SSL certificate for domain: {}, which is valid only for: {} days.",
+        display = "Expired SSL certificate for domain: {}, which is valid only for: {} days",
         _0, _1
     )]
     TLSDomainExpired(String, i32),
@@ -35,18 +36,18 @@ pub enum Unexpected {
     AddressInvalid(String, String, String),
 
     /// Curl multi handler failure
-    #[fail(display = "Curl handler failure: {}.", _0)]
+    #[fail(display = "{}", _0)]
     HandlerFailed(String),
 
     /// Http connection failed
     #[fail(
-        display = "URL: {} couldn't be reached in time frame of {} seconds.",
+        display = "URL: {} couldn't be reached in time frame of {} seconds",
         _0, _1
     )]
     HttpConnectionFailed(String, u64),
 
     /// HttpCode (url, code)
-    #[fail(display = "URL: {} returned error: {}. Expected code: {}.", _0, _1, _2)]
+    #[fail(display = "URL: {} returned error: {}. Expected code: {}", _0, _1, _2)]
     HttpCodeInvalid(String, u32, u32),
 
     /// Content - expected content not found where expected
@@ -55,22 +56,19 @@ pub enum Unexpected {
 
     /// Failed content length check
     #[fail(
-        display = "URL: {} is unable to pass minimum-content-length expectation! Actual content length: {}. Expected minimum-length: {}.",
+        display = "URL: {} is unable to pass minimum-content-length expectation! Actual content length: {}. Expected minimum-length: {}",
         _0, _1, _2
     )]
     ContentLengthInvalid(String, usize, usize),
 
     /// Check file parse error
     #[fail(
-        display = "Failed to parse check input data! Error details: \"{}\".",
+        display = "Failed to parse check input data! Error details: \"{}\"",
         _0
     )]
     CheckParseProblem(String),
 
     /// Not Implemented functionality
-    #[fail(
-        display = "Unmatched validation case for: \"{}\". Details: {}.",
-        _0, _1
-    )]
+    #[fail(display = "Unmatched validation case for: \"{}\". Details: {}", _0, _1)]
     UnmatchedValidationCase(String, String),
 }
