6168b645f62c77effd5a79668b123223befbc4d0
Daniel (dmilith) Dettlaff <dmilith@me.com>
2023-03-16 11:42:52 +0100

    new: Try linking with curl as a requirement (should enforce ssl). Also disabled 'fat' lto for release

--- build.rs
+++ build.rs
@@ -2,6 +2,5 @@
 
 fn main() {
     println!("cargo:rustc-link-lib=dylib=curl");
-    println!("cargo:rustc-link-lib=dylib=ssl");
     println!("cargo:rustc-link-search=native=/Software/Krecik/lib");
 }
