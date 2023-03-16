fc40f70b22681574c4d11588650318603fa6db73
Daniel (dmilith) Dettlaff <dmilith@me.com>
2023-03-16 12:03:57 +0100

    Revert "new: Try linking with curl as a requirement (should enforce ssl). Also disabled 'fat' lto for release"
    This reverts commit 6168b645f62c77effd5a79668b123223befbc4d0.

--- build.rs
+++ build.rs
@@ -2,5 +2,6 @@
 
 fn main() {
     println!("cargo:rustc-link-lib=dylib=curl");
+    println!("cargo:rustc-link-lib=dylib=ssl");
     println!("cargo:rustc-link-search=native=/Software/Krecik/lib");
 }
